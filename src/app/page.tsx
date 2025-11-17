ac2e5d87a1006288cda66742bd3662887e3d24b0
dmcymas <dmcymas@gmail.com>
2025-11-17 07:51:54 +0000

    dependencie cambio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import HeroSection from "./components/hero/HeroSection";
 import Projects from "./components/projects/Projects";
 import ModernContactSection from './components/contacto/Contacto';
 import ServicesSection from './components/Servicios/Servicios';
-//import Footer from "./components/footer/Footer";
+
 
 export default function Home() {
   return (
